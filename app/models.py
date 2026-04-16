9bac38abba4070df6953a92bbd7eb9913c1a5ba5
André L. C. Moreira <andrelcmoreira@proton.me>
2026-04-16 14:41:01 -0300

    adding Device and DeviceConfiguration models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    mac_address = models.CharField(max_length=17, unique=True)
+    fingerprint = models.CharField(max_length=32)
+    cfg_id = models.OneToOneField(
+        'DeviceConfiguration',
+        on_delete=models.CASCADE,
+        related_name='device',
+        null=True,
+        blank=True
+    )
+
+    class Meta:
+        db_table = 'device'
+
+
+class DeviceConfiguration(models.Model):
+    device_id = models.ForeignKey(
+        Device,
+        on_delete=models.CASCADE,
+        related_name='configurations'
+    )
+    check_cfg_interval = models.IntegerField()
+    activate_pump_interval = models.IntegerField()
+    read_sensors_interval = models.IntegerField()
+
+    class Meta:
+        db_table = 'device_cfg'
