3338ceffb9338d54eca509f163f5b301ad06217a
André L. C. Moreira <andrelcmoreira@proton.me>
2026-04-19 00:18:48 -0300

    using default DRF error handling

--- app/serializers.py
+++ app/serializers.py
@@ -1,48 +1,74 @@
+#import re
+#from django.core.validators import RegexValidator
 from rest_framework import serializers
 
-from .exceptions import InvalidField
+#from .exceptions import InvalidField
 from .models import Device, DeviceConfiguration
 
 
 class DeviceConfigurationSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(allow_blank=True)
-    check_cfg_interval = serializers.IntegerField()
-    activate_pump_interval = serializers.IntegerField()
-    read_sensors_interval = serializers.IntegerField()
+    name = serializers.CharField(
+        allow_blank=False,
+        error_messages={
+            'blank': "The 'name' field cannot be blank",
+            'required': "The 'name' field is required"
+        }
+    )
+    check_cfg_interval = serializers.IntegerField(
+        min_value=1,
+        error_messages={
+            'min_value': "The provided value for 'check_cfg_interval' must be greater than 0",
+            'required': "The 'check_cfg_interval' field is required"
+        }
+
+    )
+    activate_pump_interval = serializers.IntegerField(
+        min_value=1,
+        error_messages={
+            'min_value': "The provided value for 'activate_pump_interval' must be greater than 0",
+            'required': "The 'activate_pump_interval' field is required"
+        }
+    )
+    read_sensors_interval = serializers.IntegerField(
+        min_value=1,
+        error_messages={
+            'min_value': "The provided value for 'read_sensors_interval' must be greater than 0",
+            'required': "The 'read_sensors_interval' field is required"
+        }
+    )
 
     class Meta:
         model = DeviceConfiguration
         fields = '__all__'
 
-    def validate_name(self, value: str) -> str:
-        if not value.strip():
-            raise InvalidField('name')
-
-        return value
-
-    def validate_check_cfg_interval(self, value: int) -> int:
-        if value <= 0:
-            raise InvalidField('check_cfg_interval')
-
-        return value
-
-    def validate_activate_pump_interval(self, value: int) -> int:
-        if value <= 0:
-            raise InvalidField('activate_pump_interval')
-
-        return value
-
-    def validate_read_sensors_interval(self, value: int) -> int:
-        if value <= 0:
-            raise InvalidField('read_sensors_interval')
-
-        return value
-
 
 class DeviceSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        allow_blank=False,
+        error_messages={
+            'blank': "The 'name' field cannot be blank",
+            'required': "The 'name' field is required"
+        }
+    )
+    model = serializers.CharField(
+        allow_blank=False,
+        error_messages={
+            'blank': "The 'model' field cannot be blank",
+            'required': "The 'model' field is required"
+        }
+    )
+    mac_address = serializers.CharField(
+        allow_blank=False,
+        #pattern=r'^([0-9A-Fa-f]{2}[:]){5}([0-9A-Fa-f]{2})$',
+        max_length=17
+    )
     cfg = serializers.PrimaryKeyRelatedField(
-        queryset=DeviceConfiguration.objects.all()
+        queryset=DeviceConfiguration.objects.all(),
+        error_messages={
+            'does_not_exist': "The provided 'cfg' does not exist",
+            'required': "The 'cfg' field is required"
+        }
     )
 
     class Meta:
