1a4f154e8649029872678d6fa44cdc80f57e2813
André L. C. Moreira <andrelcmoreira@proton.me>
2026-04-30 22:17:07 -0300

    refactoring exc_handler method

--- app/error.py
+++ app/error.py
@@ -1,18 +1,23 @@
 from rest_framework.views import exception_handler
 
 
+ERROR_TYPES = [
+    'not_found',
+    'min_value',
+    'invalid',
+    'blank',
+    'required',
+    'does_not_exist'
+]
+
+
 def exc_handler(exc, context):
     reply = exception_handler(exc, context)
 
     _,val = reply.data.popitem()
     detail = val[0] if isinstance(val, list) else val
 
-    if (detail.code == 'not_found') or \
-        (detail.code == 'min_value') or \
-        (detail.code == 'invalid') or \
-        (detail.code == 'blank') or \
-        (detail.code == 'required') or \
-        (detail.code == 'does_not_exist'):
+    if detail.code in ERROR_TYPES:
         reply.data.clear()
         reply.data['msg'] = detail
 
