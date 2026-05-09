381b08f5be5395f6b98dfe09ae719eca850606c7
André L. C. Moreira <andrelcmoreira@proton.me>
2026-05-08 23:05:43 -0300

    changing app's port

--- run_app.sh
+++ run_app.sh
@@ -6,10 +6,6 @@ I=0
 while [[ $I < $MAX_RETRIES ]] ; do
     echo "Trying to connect to the database...attempt $I of $MAX_RETRIES"
     if python manage.py migrate --noinput ; then
-        echo "Applying fixtures..."
-        #python manage.py loaddata equipments
-        #python manage.py loaddata stopping_reasons
-
         echo "Running server..."
         python manage.py runserver 0.0.0.0:8000
         break
